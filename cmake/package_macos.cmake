d4ddb243fd57266920bb8750eb872c24aab82deb
phandasm <daviladsoftware@gmail.com>
2022-09-07 10:34:06 -0700

    CI: dmg -> pkg

--- cmake/package_macos.cmake
+++ cmake/package_macos.cmake
@@ -2,5 +2,7 @@ include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/package_common.cmake")
 
 set(CPACK_PACKAGE_FILE_NAME "Waveform_v${WAVEFORM_VERSION}_MacOS_${CMAKE_OSX_ARCHITECTURES}")
 
-set(CPACK_GENERATOR "DRAGNDROP")
+set(CPACK_PRODUCTBUILD_IDENTIFIER "com.github.phandasm.waveform")
+
+set(CPACK_GENERATOR "productbuild")
 include(CPack)
