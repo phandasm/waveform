e8e5024e15dba5464a4efd35b32e564767ad6327
phandasm <daviladsoftware@gmail.com>
2022-09-12 15:29:32 -0700

    relax deb package dependencies

--- cmake/package_deb.cmake
+++ cmake/package_deb.cmake
@@ -3,7 +3,8 @@ include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/package_common.cmake")
 set(CPACK_PACKAGE_FILE_NAME "Waveform_v${WAVEFORM_VERSION}_Ubuntu_x86_64")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Devin Davila <daviladsoftware@gmail.com>")
 
-set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+#set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "obs-studio (>= 27.0.0)")
 
 set(CPACK_GENERATOR "DEB")
 include(CPack)
